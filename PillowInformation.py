af1431b6f41f72ffc266b5ca06a67c5df4eac0c7
suulcoder <ef.proyectomundial@gmail.com>
2019-03-04 09:10:24 -0600

    Unuseful module

--- PillowInformation.py
+++ PillowInformation.py
@@ -2,17 +2,13 @@ from __future__ import print_function
 import sys
 from PIL import Image
 import PIL.Image
+from module import *
 
 #To open a document you must do the following:
 try:
-    original = Image.open("LogoApp.png")#Name of the document, in a specific format, in the same path of the document. 
+    original = Image.open("more.png")#Name of the document, in a specific format, in the same path of the document. 
 except:
     print("Unable to load image")
 print("The size of the Image is: ")
 print(original.format, original.size, original.mode)
-
-red="\xff\x00\x00\xff"
-green="\x00\xff\x00\xff"
-blue="\x00\x00\xff\xff"
-im = PIL.Image.frombytes("RGBA", (64,64), (red*64 + green*64 + blue*64 + red*64) * 16)
-im.save("test.png")
+pix = numpy.array(original)
