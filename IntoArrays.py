e3e44d46ef2b17355eeb76fe68d940be54da5eb8
suulcoder <ef.proyectomundial@gmail.com>
2019-03-12 08:28:00 -0600

    binary matrix

--- IntoArrays.py
+++ IntoArrays.py
@@ -9,37 +9,47 @@ import cv2
 
 #NEED TO install matplotlib and CV to work
 
-try:
-    #Get the image file into our 'original' variable
-    original = Image.open('sunspots.jpg')
-    #Get what we want
-    width, height = original.size
-    #Print data
-    print("The sie of the image is: ")
-    print(original.format, original.size, original.mode)
+#try:
+#Get the image file into our 'original' variable
+original = Image.open('sunspots.jpg')
+#Get what we want
+width, height = original.size
+original.resize((200, 200))
+#Print data
+print("The sie of the image is: ")
+print(original.format, original.size, original.mode)
 
-    #Convert to greyscale
-    mod = original.convert('L')
-    #Save in new file to compare
-    mod.save('sunspotsBW.jpg') 
+#Convert to greyscale
+mod = original.convert('L')
+#Save in new file to compare
+mod.save('sunspotsBW.jpg') 
 
-    #Create an array with the float32 values of the pixels of the image
-    array = np.asarray(mod, dtype=np.float32) 
+#Create an array with the float32 values of the pixels of the image
+array = np.asarray(mod.resize((10,10)), dtype=np.float32)
+#Fragment of the image
+respuesta = []
+b = array#[0:200, 0:200]
+for i in b:
+    lista= []
+    for pixel in i:
+        if(pixel<50):
+            lista.append(0)
+        elif(pixel>50):
+            lista.append(1)
+    respuesta.append(lista)
+#Save the fragment to compare
+#First, the array is converted into unit8 and then into an Image file
+Image.fromarray(b.astype(np.uint8)).save('fragment.jpg')
 
-    #Fragment of the image
-    b = array[360:375, 360:375]
+for i in respuesta:
+    print(i)
 
-    #Save the fragment to compare
-    #First, the array is converted into unit8 and then into an Image file
-    Image.fromarray(b.astype(np.uint8)).save('fragment.jpg')
+#Just to be sure
 
-    #Just to be sure
-    print(b)
-
-    #Use Canny Edge Detection to identify the sun
-    #edged = cv2.Canny(original, 100, 100)
-    #edged.save('CannySun.jpg')
-except:
-    print("Unable to open file...")
+#Use Canny Edge Detection to identify the sun
+#edged = cv2.Canny(original, 100, 100)
+#edged.save('CannySun.jpg')
+#except:
+ #   print("Unable to open file...")
 
 
