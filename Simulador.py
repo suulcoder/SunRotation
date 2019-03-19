1dd221669b04119ec90342b21270632b609c5d57
SuulCoder <35270907+suulcoder@users.noreply.github.com>
2019-03-19 06:52:49 -0600

    comments

--- Simulador.py
+++ Simulador.py
@@ -19,7 +19,8 @@ Proyecto 1 - Fisica 2:
 El siguiente proyecto consiste en un simulador, el objetivo de este programa es tomar fotos proporcionadas
 por el SOHO, solar and helioscopic observatory de la NASA, convirtiendolas en una estructura de datos, donde
 se realizaran ciertos calculos para calcular el angulo en que se posiciona un sunsput en el sol. Para luego
-devolver una grafica de angulo vs tiempo, para calcular la velocidad angular de dicho angulo
+devolver una tabla de angulo vs tiempo, para calcular la velocidad angular de dicho angulo es necesario realizar
+la regresion de todos las posiciones en el cambio del tiempo
 """
 
 class Matrix(object):
